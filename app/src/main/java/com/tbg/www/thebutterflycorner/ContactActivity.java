7b5d433c08f440b41eec2dd540d6c5c89ace35c6
ShaGodG <shashaankgoud@gmail.com>
2019-09-18 00:25:10 +0530

    strings

--- app/src/main/java/com/tbg/www/thebutterflycorner/ContactActivity.java
+++ app/src/main/java/com/tbg/www/thebutterflycorner/ContactActivity.java
@@ -16,6 +16,7 @@ public class ContactActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
         getSupportActionBar().setTitle("Contact Us");
+
         FloatingActionButton sharebt = findViewById(R.id.sharebtn);
         final Context contextshare = this;
         sharebt.setOnClickListener(new View.OnClickListener() {
@@ -27,7 +28,7 @@ public class ContactActivity extends AppCompatActivity {
                 String sharesubject = "Send Via";
                 shareintent.putExtra(Intent.EXTRA_SUBJECT, sharesubject);
                 shareintent.putExtra(Intent.EXTRA_TEXT, sharebody);
-                startActivity(Intent.createChooser(shareintent, "Share Using"));
+                startActivity(Intent.createChooser(shareintent , "Share Using"));
             }
         });
     }
