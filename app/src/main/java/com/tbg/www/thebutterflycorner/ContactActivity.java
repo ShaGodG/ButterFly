f48d267d42bc3feee75d72886398c233bce68cff
ShaGodG <shashaankgoud@gmail.com>
2019-09-17 22:16:45 +0530

    share btn

--- app/src/main/java/com/tbg/www/thebutterflycorner/ContactActivity.java
+++ app/src/main/java/com/tbg/www/thebutterflycorner/ContactActivity.java
@@ -1,8 +1,13 @@
 package com.tbg.www.thebutterflycorner;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ContactActivity extends AppCompatActivity {
 
@@ -11,5 +16,19 @@ public class ContactActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
         getSupportActionBar().setTitle("Contact Us");
+        FloatingActionButton sharebt = findViewById(R.id.sharebtn);
+        final Context contextshare = this;
+        sharebt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent shareintent = new Intent(Intent.ACTION_SEND);
+                shareintent.setType("text/plain");
+                String sharebody = "Send Viaa";
+                String sharesubject = "Send Via";
+                shareintent.putExtra(Intent.EXTRA_SUBJECT, sharesubject);
+                shareintent.putExtra(Intent.EXTRA_TEXT, sharebody);
+                startActivity(Intent.createChooser(shareintent, "Share Using"));
+            }
+        });
     }
 }
